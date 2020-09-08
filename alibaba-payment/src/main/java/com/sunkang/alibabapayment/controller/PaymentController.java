aea2272065076d122a2684f131b8db02faae3ee6
pactera-228 <183657170@qq.com>
2020-09-08 14:01:36 +0800

    nacos配置中心

--- alibaba-payment/src/main/java/com/sunkang/alibabapayment/controller/PaymentController.java
+++ alibaba-payment/src/main/java/com/sunkang/alibabapayment/controller/PaymentController.java
@@ -2,6 +2,7 @@ package com.sunkang.alibabapayment.controller;
 
 import cn.hutool.core.util.StrUtil;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
  * 支付
  */
 @RestController
+@RefreshScope
 public class PaymentController {
 
     @Value("${server.port}")
     private String port;
 
+    @Value("${config.info}")
+    private String configInfo;
+
     @GetMapping(value = "/peyment/{id}")
     public String peyment(@PathVariable(value = "id") Long id) {
         return StrUtil.format("alibaba payment port:{} id:{}", port, id);
+    }
+
+    @GetMapping(value = "/configInfo")
+    public String configInfo(){
+        return this.configInfo;
     }
 }
